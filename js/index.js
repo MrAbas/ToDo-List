9ad3b7d32a51e2b027222a42e861526a0f6da474
MrAbas <abasov2016@list.ru>
2023-11-16 17:50:44 +0300

    начал писать логику, в консоль выводит undefined

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const modal = document.querySelector(".modal");
 const btnAdd = document.getElementById("btn_add");
 const btnClose = document.getElementById("btn_close");
 const btnSecondary = document.getElementById("btn_secondary");
+const btnApply = document.getElementById("btnApply");
 
 btnAdd.onclick = function () {
   modal.style.display = "block";
@@ -13,4 +14,12 @@ btnClose.onclick = function () {
 
 btnSecondary.onclick = function () {
   modal.style.display = "none";
+};
+
+btnApply.onclick = function () {
+  const modalInputValue = document.getElementById("modalInput").value;
+  console.log(modalInputValue);
+
+  modal.style.display = "none";
+  document.getElementById("modalInput").value = "";
 };
