7cc2e27dd1e77c37ac3d7e7c98730be7d44d6253
MrAbas <abasov2016@list.ru>
2023-11-19 18:03:06 +0300

    ..

--- js/index.js
+++ js/index.js
@@ -2,7 +2,27 @@ const modal = document.querySelector(".modal");
 const btnAdd = document.getElementById("btn_add");
 const btnClose = document.getElementById("btn_close");
 const btnSecondary = document.getElementById("btn_secondary");
-const btnApply = document.getElementById("btnApply");
+const modalInput = document.getElementById("modalInput"); //del
+const btnApply = document.getElementById("btnApply"); //del
+
+if (localStorage.todo) {
+  let localStorageItems = JSON.parse(localStorage.todo);
+  const list = document.getElementById("list");
+
+  for (let i = 0; i < localStorageItems.length; i++) {
+    list.insertAdjacentHTML(
+      "beforeend",
+      `<li id='${localStorageItems[i].id}'  class="note">
+    <input type="checkbox" class="checkbox_note" />
+    <h2 class="text_note">${localStorageItems[i].text}</h2>
+    <span class="btns-note">
+      <button class="btn_change"></button>
+      <button class="btn_deleted" onclick="onDelete('${localStorageItems[i].id}')" ></button>
+    </span>
+  </li>`
+    );
+  }
+}
 
 btnAdd.onclick = function () {
   modal.style.display = "block";
@@ -17,31 +37,44 @@ btnSecondary.onclick = function () {
 };
 
 const onDelete = (id) => {
-  const list_item = document.getElementById(id);
-  list_item.remove();
+  const itemRemove = document.getElementById(id);
+  itemRemove.remove();
 };
 
 btnApply.onclick = function () {
-  /* function getRandomInt(max) {
+  function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
-  const newId = getRandomInt(10000); */
-
-  const modalInputValue = document.getElementById("modalInput").value;
+  const newId = getRandomInt(10000);
+  const modalInputValue = modalInput.value;
   const list = document.getElementById("list");
 
   list.insertAdjacentHTML(
     "beforeend",
-    `<li id=list_item class="note">
-    <input type="checkbox" class="checkbox_note" />
-    <h2 class="text_note">${modalInputValue}</h2>
-    <span class="btns-note">
-      <button class="btn_change"></button>
-      <button class="btn_deleted" onclick="onDelete('list_item')"></button>
-    </span>
-  </li> `
+    `<li id='${newId}' class="note">
+  <input type="checkbox" class="checkbox_note" />
+  <h2 class="text_note">${modalInputValue}</h2>
+  <span class="btns-note">
+    <button class="btn_change"></button>
+    <button class="btn_deleted" onclick="onDelete('${newId}')"></button>
+  </span>
+</li>`
   );
 
   modal.style.display = "none";
-  document.getElementById("modalInput").value = "";
+  modalInput.value = "";
+
+  let localStorageItems = [];
+  if (localStorageItems?.length) {
+    localStorageItems.push({ id: newId, text: modalInputValue });
+    localStorage.todo = JSON.stringify(localStorageItems);
+  } else {
+    localStorage.todo = JSON.stringify([{ id: newId, text: modalInputValue }]);
+  }
 };
+
+btnAdd.addEventListener("click", openInput);
+
+function openInput() {
+  modalInput.focus();
+}
