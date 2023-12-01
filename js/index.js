328ad1d53dfb24ba65337e8d31b4006b18fd92ef
MrAbas <abasov2016@list.ru>
2023-12-01 23:43:56 +0300

    добавили логику на кнопку редактирования

--- js/index.js
+++ js/index.js
@@ -24,9 +24,9 @@ const addChildToList = (id, text) => {
     "beforeend",
     `<li id=${id} class="note">
   <input type="checkbox" class="checkbox_note" />
-  <h2 class="text_note">${text}</h2>
+  <h2 id="${id}-text" class="text_note">${text}</h2>
   <span class="btns-note">
-    <button class="btn_change"></button>
+    <button onclick = "onBtnChange(${id}, ${text})" class="btn_change"></button>
     <button class="btn_deleted" onclick = "onDelete(${id})"></button>
   </span>
 </li>`
@@ -56,7 +56,7 @@ const onDelete = function (id) {
   const itemRemove = document.getElementById(id);
   itemRemove.remove();
   const newArr = getListFromStorage().filter((toDoListItem) => {
-    return toDoListItem.id === Number(id);
+    return toDoListItem.id !== Number(id);
   });
   setListToStorage(newArr);
 };
@@ -107,3 +107,48 @@ inputSearch.addEventListener("input", function (e) {
     }
   }
 });
+
+let inputOpen = false; // следим за нажатием
+
+function onBtnChange(id, text) {
+  if (inputOpen === false) {
+    //проверка было ли нажатие
+    const textValue = document.getElementById(id + "-text");
+    const input = document.createElement("input");
+    const listItem = document.getElementById(id);
+
+    input.className = "input_change";
+    input.placeholder = textValue.textContent;
+
+    listItem.replaceChild(input, textValue); //меняем текст на input
+
+    inputOpen = true; //запретили повторное нажатие
+
+    input.addEventListener("keydown", (e) => {
+      //отслеживаем нажатие
+      if (e.key === "Enter") {
+        openInput = false; //разрешили нажатие
+        // если  Enter
+        textValue.textContent = e.target.value;
+        listItem.replaceChild(textValue, input); //меняем input на текст
+        //1 записываем в переменную лк
+        let localItems = getListFromStorage();
+        //2 находим в нем объект по id
+        /*        localItems = localItems.map((localItem) => {
+          if (localItem.id === id) {
+            localItem.value = e.target.value;
+          }
+          return localItem
+        }); */
+        for (let i = 0; i < localItems.length; i++) {
+          if (localItems[i].id === id) {
+            localItems[i].value = e.target.value;
+          }
+        }
+        //3 меняем в нем текст на новый
+        setListToStorage(localItems);
+        //4 записываем в лк новый массив
+      }
+    });
+  }
+}
